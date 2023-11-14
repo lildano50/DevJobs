1f18daee9061c9d139d3aacec68512849672931f
Daniel Park <danielapark87@gmail.com>
2023-11-13 20:22:05 -0600

    established database and tables

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Jobs = require('./Jobs');
+
+User.hasMany(Jobs, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Jobs.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Jobs };
